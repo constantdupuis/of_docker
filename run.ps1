9d436d79996385a4189c29614a8555b6fa60016f
Constant Dupuis <31339604+constantdupuis@users.noreply.github.com>
2020-02-15 11:01:47 +0100

    Update readme and script
    Add instruction in readme
    Update run.ps1 to seek it's cwd him self
    retreive OF sample compiled inside the container

--- run.ps1
+++ run.ps1
@@ -1,1 +1,7 @@
-docker run -it -v 'D:/Constant Dupuis/Dev/docker/of_docker/apps:/home/dev/OF/apps:rw' of_emscript 
+# create arguments base on current location
+$cwd = Get-Location
+$cwd = "{0}/apps:/home/dev/OF/apps:rw" -f $cwd
+
+#Write-Host $cwd
+
+docker run -it -v $cwd of_emscript 
